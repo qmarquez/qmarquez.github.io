e546853cf3973cd001e43065353c09052fec5fa4
Quimey <zerox_94_mm@hotmail.com>
2020-04-05 17:22:47 -0300

    Deploy builded react app.

---
+++ precache-manifest.7b618ef5939b5f990e502f0c29b5d856.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34b39a263643ca76a55fd0beb7fb3d21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda928e2bc2561e240e9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e7eb89a31ea48bb60044",
+    "url": "/static/css/main.84df6afc.chunk.css"
+  },
+  {
+    "revision": "bda928e2bc2561e240e9",
+    "url": "/static/js/2.f8d7a004.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f8d7a004.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7eb89a31ea48bb60044",
+    "url": "/static/js/main.6168cc23.chunk.js"
+  },
+  {
+    "revision": "d502cf76f26c577e704d",
+    "url": "/static/js/runtime-main.f654b18c.js"
+  },
+  {
+    "revision": "91013f6797bdd329ed3d043789ffae78",
+    "url": "/static/media/argentina.91013f67.svg"
+  },
+  {
+    "revision": "6160e9db9f0414a40567fd3c1fb9f44f",
+    "url": "/static/media/as.6160e9db.svg"
+  },
+  {
+    "revision": "36938bbe277961386e7e309dcb30cbd8",
+    "url": "/static/media/espana.36938bbe.svg"
+  },
+  {
+    "revision": "0dd81fce2e3edbae20c512e1e1aa42f9",
+    "url": "/static/media/profile.0dd81fce.jpg"
+  },
+  {
+    "revision": "36ab476e5e55f496749ee61897a9cfb5",
+    "url": "/static/media/usa.36ab476e.svg"
+  }
+]);
